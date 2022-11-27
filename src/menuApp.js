dce75ef12b0e59737894b3bf9facadd093edc61f
shjk0531 <shinjk0531@gmail.com>
2022-11-27 19:52:10 +0900

    menu update

--- src/menuApp.js
+++ src/menuApp.js
@@ -1,5 +1,29 @@
 function App() {
-  return <div></div>;
+  // 메뉴 데이터(이후 수정 예정)
+  const menuData = [
+    {
+      name: "메뉴1",
+      img: "##",
+    },
+    {
+      name: "메뉴2",
+      img: "##",
+    },
+  ];
+
+  const menuRendering = () => {
+    const menus = [];
+    for (let index = 0; index < menuData.length; index++) {
+      menus.push(
+        <form>
+          <img src={menuData[index].img} alt={menuData[index].name} />
+          <p>{menuData[index].name}</p>
+        </form>
+      );
+    }
+  };
+
+  return <div>{menuRendering()}</div>;
 }
 
 export default App;
