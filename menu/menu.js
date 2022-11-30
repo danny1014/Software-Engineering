0d6908942d3b897e9457cb83f441509fd4bfefeb
shjk0531 <shinjk0531@gmail.com>
2022-11-30 21:31:52 +0900

    update menu

--- menu/menu.js
+++ menu/menu.js
@@ -3,7 +3,7 @@ const menuData = [
     name: "메뉴1",
     img: "./img/menuImg.png",
     price: 5000,
-    information: "메뉴 1 상세정보",
+    information: "메뉴1 상세정보",
   },
   {
     name: "메뉴2",
@@ -11,15 +11,62 @@ const menuData = [
     price: 7000,
     information: "메뉴2 상세정보",
   },
+  {
+    name: "메뉴3",
+    img: "./img/menuImg.png",
+    price: 5000,
+    information: "메뉴3 상세정보",
+  },
+  {
+    name: "메뉴4",
+    img: "./img/menuImg.png",
+    price: 7000,
+    information: "메뉴4 상세정보",
+  },
+  {
+    name: "메뉴5",
+    img: "./img/menuImg.png",
+    price: 5000,
+    information: "메뉴5 상세정보",
+  },
+  {
+    name: "메뉴6",
+    img: "./img/menuImg.png",
+    price: 7000,
+    information: "메뉴6 상세정보",
+  },
+  {
+    name: "메뉴7",
+    img: "./img/menuImg.png",
+    price: 5000,
+    information: "메뉴7 상세정보",
+  },
+  {
+    name: "메뉴8",
+    img: "./img/menuImg.png",
+    price: 7000,
+    information: "메뉴8 상세정보",
+  },
+  {
+    name: "메뉴9",
+    img: "./img/menuImg.png",
+    price: 5000,
+    information: "메뉴9 상세정보",
+  },
+  {
+    name: "메뉴10",
+    img: "./img/menuImg.png",
+    price: 7000,
+    information: "메뉴10 상세정보",
+  },
 ];
 
 function menuInit() {
-  let menus = document.getElementById("menus");
-  console.log(menus);
+  const menus = document.getElementById("menus");
 
   for (let i = 0; i < menuData.length; i++) {
-    const menuName = `<p class="menuName">${menuData[i].name}</p>`;
-    const menuImg = `<img class="menuImg" src='${menuData[i].img}' alt="${menuData[i].name} 이미지">`;
+    const menuName = `<p class="menuName" id="name${menuData[i].name}">${menuData[i].name}</p>`;
+    const menuImg = `<img class="menuImg" id="img${menuData[i].name}" src='${menuData[i].img}' alt="${menuData[i].name} 이미지">`;
 
     menus.innerHTML =
       menus.innerHTML +
@@ -31,15 +78,25 @@ function menuInit() {
 
   const onMenuClick = (event) => {
     console.log("click");
+    const id = event.target.id;
+
     for (let i = 0; i < menuData.length; i++) {
-      console.log(`search ${i}`);
-      console.log(event.target);
-      console.log(menuData[i].name);
-      if (event.target.id === menuData[i].name) {
-        console.log("search" + menuData[i]);
-        const menuName = `<h1 class="popupName">${menuData[i].name}</h1>`;
-        const menuImg = `<img class="popupImg" src='${menuData[i].img}' alt="${menuData[i].name}">`;
-        const menuInformation = `<p class="popupInfo">${menuData[i].information}</p>`;
+      const element = menuData[i];
+
+      console.log("event target: " + event.target);
+      console.log("element name: " + element.name);
+      console.log("id: " + id + "\n");
+      console.log("id" + element.name);
+
+      if (
+        id === element.name ||
+        id === "img" + element.name ||
+        id === "name" + element.name
+      ) {
+        console.log("search" + element);
+        const menuName = `<h1 class="popupName">${element.name}</h1>`;
+        const menuImg = `<img class="popupImg" src='${element.img}' alt="${element.name}">`;
+        const menuInformation = `<p class="popupInfo">${element.information}</p>`;
 
         popup.innerHTML = menuImg + menuName + menuInformation;
         return;
